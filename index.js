2fa8072f9edbef6a5b68a957b0ee36d61cfbf582
Matt Helton <MJoelHelton@me.com>
2018-12-06 20:41:22 -0500

    Adds the removal of the 'choice' element everytime the Generate button is clicked and adds CSS styling.

--- index.js
+++ index.js
@@ -18,34 +18,30 @@ let choices = ['“Beginners guide to...”',
   'Update old content',
   'Write a review/recommend a product, service, etc',
   '“x things to learn from...” (this could be a conference, event, TV show, movie, anything)']
-
+// Generates a random number between 0-19
 function randomizer () {
-  let number = Math.floor((Math.random() * 20))
-  console.log(number)
-  return number
+  return Math.floor((Math.random() * 20))
 }
-
+// Calls the randomizer and uses the number it returns to pull out a string from the array that has the corosponding index number (0-19)
+//  Then calls renderChoice function to render the string
 function picker () {
   let choice = randomizer()
   renderChoice(choices[choice])
-
-  function renderChoice (x) {
-    let div = document.createElement('div')
-    let result = document.createTextNode(x)
-
-    div.className = 'choice'
-    div.appendChild(result)
-    let currentDiv = document.getElementById('parent')
-    document.body.insertBefore(div, currentDiv)
-    let formerChoice = document.querySelector('.choice')
-
-    if (!formerChoice) {
-    } else {
-      formerChoice.classList.add('hidden')
-    }
-}
 }
 
-let roll = document.querySelector('.roll')
+// Creates a new h1, gives it the classname 'choice',inserts the chosen string into the new h1,
+function renderChoice (string) {
+  let removableh1 = document.getElementById('choice')
+  let h1 = document.createElement('h1')
+  let result = document.createTextNode(string)
+  let currenth1 = document.getElementById('parent')
 
-roll.addEventListener('click', picker)
+  removableh1.parentNode.removeChild(removableh1)
+  h1.id = 'choice'
+  h1.appendChild(result)
+  document.body.insertBefore(h1, currenth1)
+}
+
+let generator = document.querySelector('.generate')
+
+generator.addEventListener('click', picker)
