3e0885b6362faea9e130f965db3d7dec776f291e
Matt Helton <MJoelHelton@me.com>
2018-12-08 09:52:57 -0500

    Fixes backlog issue by adding arguments to the functions, and then creating the variable inside the renderIdea function so that nothing is lost. Also creates game function that calls the render.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // Created this simple app for my partner's work. They just wanted a button that generates a random prompt.
 
-//    As it stands, it's fully functional but everytime I generate a new h1 wih and "idea" in it, the idea is appended to the previous one.
+//    As it stands, it's fully functional but everytime I generate a new h1 with an "idea" in it, the idea is appended to the previous one.
 
 const choices = ['“Beginners guide to...”', 'Buying/gift guide', 'Checklist', 'Comparison piece', 'Create a calendar of upcoming events', 'FAQ', 'Flow chart', '“How to...”', 'Interview industry experts (inside or outside your company)', 'Links roundup', 'List of things to do/never do', 'Mythbusting article', 'Predict future trends', 'Quote piece', 'The biggest influences/influencers', 'Timeline', 'Top 10 list', 'Update old content', 'Write a review/recommend a product, service, etc', '“x things to learn from...” (this could be a conference, event, TV show, movie, anything)']
 const generator = document.querySelector('.generate')
-var currentH1 = document.getElementById('parent')
-var h1 = document.createElement('h1')
 
 // Generates a random number between 0-19
 function randomizer () {
@@ -16,33 +14,40 @@ function chooseIdea () {
   return choices[randomizer()]
 }
 // Removes idea render everytime Generate is clicked. Uses setRemovableH1 as argument to find the element to delete
-function removeH1 () {
-  let removableH1 = document.getElementById('idea')
-  removableH1.parentNode.removeChild(removableH1)
+function removeH1 (el) {
+  el.parentNode.removeChild(el)
 }
 
 // Takes the idea chosen from the array by using chooseIdea as an argument, and adds the text of the idea to the h1 element
-function insertIdeaTextInH1 (string) {
+function insertIdeaTextInH1 (string, el) {
   let ideaText = document.createTextNode(string)
-  return h1.appendChild(ideaText)
+  return el.appendChild(ideaText)
 }
 
 // Gives the h1 element the id 'choice'
-function giveH1Id () {
-  h1.id = 'idea'
+function giveH1Id (el) {
+  el.id = 'idea'
 }
 
 // Takes in the h1 element and 'current element' as arguments, inserting the new h1 element in the dom before the 'current element'
 function insertIdea (el, currentEl) {
+  console.log(el)
   return document.body.insertBefore(el, currentEl)
 }
 
 // Calls all of the functions in order
 function renderIdea () {
-  removeH1()
-  insertIdeaTextInH1(chooseIdea())
-  giveH1Id()
+  let currentH1 = document.getElementById('parent')
+  let h1 = document.createElement('h1')
+  let removableH1 = document.getElementById('idea')
+  removeH1(removableH1)
+  giveH1Id(h1)
+  insertIdeaTextInH1(chooseIdea(), h1)
   insertIdea(h1, currentH1)
 }
+
+function ideationGame () {
+  renderIdea()
+}
 // Adds event listener to the Generate button so that the renderIdea function is called everytime it is clicked
-generator.addEventListener('click', renderIdea)
+generator.addEventListener('click', ideationGame)
